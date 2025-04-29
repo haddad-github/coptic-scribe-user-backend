63ff105a7efb8cf5de5458f67563a4a010bac238
haddad-github <68672661+haddad-github@users.noreply.github.com>
2025-04-28 23:14:33 -0400

    removed call to .env

--- src/main/java/coptic/user_api/UserApiApplication.java
+++ src/main/java/coptic/user_api/UserApiApplication.java
@@ -2,7 +2,6 @@ package coptic.user_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class UserApiApplication {
@@ -12,12 +11,6 @@ public class UserApiApplication {
      * @param args Command-line arguments
      */
     public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.load(); //load .env file
-    System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-    System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-    System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
-
-    SpringApplication.run(UserApiApplication.class, args);
-	}
-
+        SpringApplication.run(UserApiApplication.class, args);
+    }
 }
